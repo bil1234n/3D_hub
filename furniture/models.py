47ffd146e11f3337ed30d8f7b24ad07a5bff0b07
bil1234n <bilalnesru123456789@gmail.com>
2026-02-28 08:34:27 +0300

    Update models.py

--- furniture/models.py
+++ furniture/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -13,14 +14,23 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    
+    # Images will automatically use the DEFAULT_FILE_STORAGE (Standard Media)
     main_image = models.ImageField(upload_to='products/images/')
-    model_3d = models.FileField(upload_to='products/models/')
+    
+    # 3D Models are explicitly forced to use Raw Storage
+    model_3d = models.FileField(
+        upload_to='products/models/',
+        storage=RawMediaCloudinaryStorage()
+    )
 
     def __str__(self):
         return self.name
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='gallery', on_delete=models.CASCADE)
+    
+    # Gallery images also use standard Media storage
     image = models.ImageField(upload_to='products/gallery/')
 
     def __str__(self):
